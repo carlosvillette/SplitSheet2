d445d4ef68523746d6f7cd9f5d00e97efab3f69d
Guy Carlos Villette <guycarlosvillette@hon-2016238.local>
2020-02-08 20:47:38 -0500

    Initial Commit

---
+++ SmartDev/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  SmartDev
+//
+//  Created by Guy Carlos Villette on 2/8/20.
+//  Copyright © 2020 Guy Carlos Villette. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
